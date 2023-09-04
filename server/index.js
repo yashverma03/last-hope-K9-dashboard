7f76c51d1f29a892292021c12ed1709189b35b1e
yashverma03 <vermayash2003@gmail.com>
2023-09-04 12:53:07 +0530

    setting up json-server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-import express from 'express';
-import cors from 'cors';
+import jsonServer from 'json-server';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const server = jsonServer.create();
+
+server.use(jsonServer.defaults());
+
+const router = jsonServer.router('db.json');
+server.use(router);
 
 const port = 4000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
