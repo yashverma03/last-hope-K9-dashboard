1aef282d7133fa598fa667ac7b4cd5aef25eabc6
yashverma03 <vermayash2003@gmail.com>
2023-09-04 14:01:56 +0530

    added all components to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
 import LivesSaved from './components/LivesSaved/LivesSaved';
+import AdoptByAge from './components/AdoptByAge/AdoptByAge';
+import AdoptOverTime from './components/AdoptOverTime/AdoptOverTime';
+import Characteristics from './components/Characteristics/Characteristics';
+import TopBreeds from './components/TopBreeds/TopBreeds';
 
 const App = () => {
   return (
-    <div>
-      <LivesSaved />
+    <div className='app'>
+      <div className='component'>
+        <LivesSaved />
+      </div>
+
+      <div className='component'>
+        {/* <AdoptByAge /> */}
+        <AdoptOverTime />
+      </div>
+
+      <div className='component'>
+        {/* <Characteristics /> */}
+        {/* <TopBreeds /> */}
+      </div>
     </div>
   );
 };
